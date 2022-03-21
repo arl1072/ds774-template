99894675c305a0df25eaeee18ea2c14315ecd2c3
arl1072 <arl1072@wildcats.unh.edu>
2022-03-21 16:30:20 -0400

    new

--- app.py
+++ app.py
@@ -70,21 +70,23 @@ def admin():
 def insertissue():
     message = ''
     if request.method == 'POST':
-        fname = request.form['fname']
-        lname = request.form['lname']
-        status = request.form['status']
-        message = request.form['message']
-        result = contact_form(fname, lname, status, message)
-        if result:
-            return render_template('insertissue.html', message='Thank you for your submission')
+        if request.form.get("issue"):
+            fname = request.form['fname']
+            lname = request.form['lname']
+            status = request.form['status']
+            message = request.form['message']
+            result = contact_form(fname, lname, status, message)
+            if result:
+                return render_template('insertissue.html', message='Thank you for your submission')
+            else:
+                return render_template('insertissue.html', message='Error with submission')
         else:
-            return render_template('insertissue.html', message='Error with submission')
-    else:
-        return render_template('insertissue.html', message=message)
+            return render_template('insertissue.html', message=message)
 
     # if form was logout button, end user session
-    if request.form.get('admin')  == 'Logout':
+    elif request.form.get('admin')  == 'Logout':
         session.pop('user_id')
+        return render_template('admin.html')
 
 
 @app.route("/register", methods=['GET', 'POST'])
